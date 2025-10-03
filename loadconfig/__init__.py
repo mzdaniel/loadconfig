20aa53724cbb452d68ddc6823b7010c89927559d
Daniel Mizyrycki <mzdaniel@glidelink.net>
2025-10-03 07:49:23 -0700

    Release 0.2.0
    Signed-off-by: Daniel Mizyrycki <mzdaniel@glidelink.net>

--- loadconfig/__init__.py
+++ loadconfig/__init__.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 __all__ = ['Config', 'Odict', '__version__']
 
 __author__ = 'Daniel Mizyrycki'
-__version__ = '0.2.0.dev1'
+__version__ = '0.2.0'
 
 from itertools import count
 from .lib import (Odict, delregex, dfl, findregex, flatten,
